855d1c3fc2ec1e1ad6ac5a07b112f31c17824dc4
Raj Sellappan <rajsell@microsoft.com>
2018-02-07 11:11:51 -0800

    Adding more logging

--- RInterop/DependencyFactory.cs
+++ RInterop/DependencyFactory.cs
@@ -10,7 +10,11 @@ namespace RInterop
             var container = new UnityContainer();
             Container = container;
             Container.RegisterInstance<ILogger>(new TraceLogger("%temp%", "RInterop"));
-            Resolve<ILogger>().LogInformation("Completed registering dependencies");
+            var logger = Resolve<ILogger>();
+            var version = Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(AssemblyFileVersionAttribute)) as AssemblyFileVersionAttribute;
+            logger.LogInformation(string.Format("Starting RInterop {0}", version != null ? version.Version : "N/A"));
+            logger.LogInformation(string.Format("Install Location {0}", System.AppDomain.CurrentDomain.BaseDirectory));
+            logger.LogInformation("Completed registering dependencies");
         }
 
         public static IUnityContainer Container { get; private set; }
